6183a9176f13fd1b149420ea1c07ac8e5e20def5
Saad Mecci <saadmecci@yahoo.com>
2020-07-07 16:25:52 -0500

    Adds mongodb connection and dotenv file

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,53 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 const axios = require('axios');
+require('dotenv').config();
 const port = process.env.PORT || 8000;
 app.use(express.static(path.join(__dirname, 'build')));
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const url = 'mongodb://127.0.0.1:27017';
+const dbName = 'dev';
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true}))
+
+
+function addSong(body) {
+  console.log(body)
+  MongoClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
+    assert.equal(null, err);
+    const db = client.db(dbName);
+
+    addData(body, db, function() {
+      client.close();
+    })
+  });
+}
+
+const addData = function (body, db, callback) {
+  const songCollection = db.collection("playlist");
+  songCollection.insertOne(
+      {body},
+      function (err, result) {
+      assert.equal(err, null);
+      assert.equal(1, result.result.n);
+      assert.equal(1, result.ops.length);
+      console.log("inserted a song");
+      callback(result);
+    }
+  );
+}
+
+app.post('/add-song', async (req, res) => {
+  addSong(req.body)
+  res.json({
+    status: 200,
+    data: req.body
+  })
+})
 
 app.get('/', function (req, res) {
   const directoryPath = __dirname.substring(0, __dirname.indexOf("/backend")) + "/frontend";
@@ -12,11 +57,6 @@ app.get('/', function (req, res) {
 });
 
 app.listen(port, function() { console.log("server is running on", port)});
-
-const clientObject = {
-  id: "375fa4cd18df42d6a1a0e4204620ea80",
-  secret: "ae64e89eb0834af68e4c707cf977ed56"
-};
 
 app.get('/token', function (req, res) {
   axios({
@@ -30,8 +70,8 @@ app.get('/token', function (req, res) {
       "Content-Type": "application/x-www-form-urlencoded"
     },
     auth: {
-      username: clientObject.id,
-      password: clientObject.secret
+      username: process.env.SPOTIFY_CLIENT_ID,
+      password: process.env.SPOTIFY_CLIENT_SECRET
     }
   }).then((results) => {
     if (results.status === 200) {
